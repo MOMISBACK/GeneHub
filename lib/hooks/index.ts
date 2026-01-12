48200be4aca782c4d70a16f707872ff80814b9ec
MOMISBACK <46964435+MOMISBACK@users.noreply.github.com>
2026-01-12 22:38:59 +0100

    Delete lib directory

--- lib/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Custom hooks for GeneHub
- */
-
-export { useGeneData, type UseGeneDataResult } from './useGeneData';
-export { useFunctionReferences, type UseFunctionReferencesResult } from './useFunctionReferences';
-export { useNotes } from './useNotes';
-export { useInbox, type UseInboxResult, type UseInboxOptions } from './useInbox';
